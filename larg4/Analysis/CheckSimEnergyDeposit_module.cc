197c8eb6d2479771efd064a71e7a2c18fe54cb83
Hans-Joachim Wenzel <wenzel@fnal.gov>
2018-06-20 09:22:01 -0500

    refactoring out the larsoft dependent part of artg4tk

---
+++ larg4/Analysis/CheckSimEnergyDeposit_module.cc
@@ -0,0 +1,98 @@
+//
+//               __        __ __  __  __  
+//   ____ ______/ /_____ _/ // / / /_/ /__
+//  / __ `/ ___/ __/ __ `/ // /_/ __/ //_/
+// / /_/ / /  / /_/ /_/ /__  __/ /_/ ,<   
+// \__,_/_/   \__/\__, /  /_/  \__/_/|_|  
+//               /____/                  
+//
+// artg4tk: art based Geant 4 Toolkit
+// 
+//=============================================================================
+// CheckSimEnergyDepositHit_module.cc: Analyzer module that demonstrates access to 
+// Calorimeter hits 
+// and makes some histograms
+// 
+// Author: Hans Wenzel (Fermilab)
+//=============================================================================
+#include "larg4/Analysis/CheckSimEnergyDeposit_module.h"
+#include "art/Framework/Core/EDAnalyzer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+#include <cmath>   
+larg4::CheckSimEnergyDeposit::CheckSimEnergyDeposit(fhicl::ParameterSet const& p) :
+art::EDAnalyzer(p),
+_hnHits(0),
+_hEdep(0),
+_hnumPhotons(0),
+_hLandauPhotons(0),
+_hLandauEdep(0),
+_hSteplength(0),
+_ntuple(0) {
+}
+
+void larg4::CheckSimEnergyDeposit::beginRun(const art::Run& thisRun) {
+
+}
+
+void larg4::CheckSimEnergyDeposit::beginJob() {
+    art::ServiceHandle<art::TFileService> tfs;
+    _hnHits = tfs->make<TH1F>("hnHits", "Number of SimEnergyDeposits", 300, 0, 0);
+    _hEdep = tfs->make<TH1F>("hEdep", "Energy deposition in SimEnergyDeposits", 100,0.,0.02);
+    _hnumPhotons = tfs->make<TH1F>("hnumPhotons", "number of photons per  SimEnergyDeposit", 100,0.,500.);
+    _hLandauPhotons= tfs->make<TH1F>("hLandauPhotons", "number of photons/cm", 100,0.,2000000.);
+    _hLandauEdep= tfs->make<TH1F>("hLandauEdep", "Edep/0.5cm", 100,0.,40.);
+    _hSteplength= tfs->make<TH1F>("hSteplength", "geant 4 step length", 100,0.,0.05);
+    _ntuple = tfs->make<TNtuple>("ntuple","Demo ntuple",
+			  "Event:Edep:em_Edep:nonem_Edep:xpos:ypos:zpos:time");
+
+} // end beginJob
+
+void larg4::CheckSimEnergyDeposit::analyze(const art::Event& event) {
+  typedef std::vector< art::Handle<sim::SimEnergyDepositCollection> > HandleVector;
+    HandleVector allSims;
+    event.getManyByType(allSims);
+    double sumE = 0.0;
+    double sumPhotons=0.0;
+    for (HandleVector::const_iterator i = allSims.begin(); i != allSims.end(); ++i) {
+      const sim::SimEnergyDepositCollection & sims(**i);
+       cout << " SimEnergyDeposit collection size:  " << sims.size() << endl;
+       sumPhotons=0.0;
+       sumE = 0.0;
+       _hnHits->Fill(sims.size());
+       for (sim::SimEnergyDepositCollection::const_iterator j = sims.begin(); j != sims.end(); ++j) {
+	 const sim::SimEnergyDeposit& hit = *j;
+	  _hEdep->Fill(hit.Energy());
+	  // sum up energy deposit in a 5mm slice of liquid Argon. 
+	  if (std::abs(hit.EndZ()/CLHEP::cm)<0.25) {
+	      sumPhotons= sumPhotons + hit.NumPhotons();
+	      sumE= sumE +hit.Energy();
+	    }
+	  _hnumPhotons->Fill( hit.NumPhotons());
+	  _hEdep->Fill( hit.Energy());
+	  std::cout<< "length:   "<<hit.StepLength()<< "   "<<hit.StepLength()/CLHEP::cm <<std::endl;
+	  _hSteplength->Fill( hit.StepLength());      
+	  /*
+	    _ntuple->Fill(event.event(),
+			  hit.GetEdep(),
+			  hit.GetEdepEM(),
+			  hit.GetEdepnonEM(),
+			  hit.GetXpos(),
+			  hit.GetYpos(),
+			  hit.GetZpos(),
+			  hit.GetTime());
+	  */
+        }
+	_hLandauPhotons->Fill(sumPhotons);
+	_hLandauEdep->Fill(sumE / CLHEP::MeV);
+	
+    }
+} // end analyze
+
+void larg4::CheckSimEnergyDeposit::endJob() {
+
+}// end endJob
+
+using larg4::CheckSimEnergyDeposit;
+
+DEFINE_ART_MODULE(CheckSimEnergyDeposit)
---
+++ larg4/Analysis/CheckSimEnergyDeposit_module.h
@@ -0,0 +1,68 @@
+//
+//               __        __ __  __  __  
+//   ____ ______/ /_____ _/ // / / /_/ /__
+//  / __ `/ ___/ __/ __ `/ // /_/ __/ //_/
+// / /_/ / /  / /_/ /_/ /__  __/ /_/ ,<   
+// \__,_/_/   \__/\__, /  /_/  \__/_/|_|  
+//               /____/                  
+//
+// artg4tk: art based Geant 4 Toolkit
+// 
+//=============================================================================
+// CheckSimEnergyDeposit_module.h: Analyzer module that demonstrates access to 
+// SimEnergyDepositHit hits 
+// and makes some histograms
+// 
+// Author: Hans Wenzel (Fermilab)
+//=============================================================================
+#ifndef CHECKSIMENERGYDEPOSIT_MODULE_HH
+#define	CHECKSIMENERGYDEPOSIT_MODULE_HH
+// art Framework includes.
+#include "art/Framework/Core/EDAnalyzer.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Services/Optional/TFileService.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Run.h"
+#include "art/Framework/Services/Optional/TFileDirectory.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "art/Framework/Principal/Provenance.h"
+#include "artg4tk/services/DetectorHolder_service.hh"
+#include "lardataobj/Simulation/SimEnergyDeposit.h"
+// Root includes.
+#include "TFile.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TNtuple.h"
+#include "TDirectory.h"
+// Other includes.
+#include "CLHEP/Units/SystemOfUnits.h"
+
+using namespace std;
+namespace larg4 {
+    class CheckSimEnergyDeposit;
+}
+
+class larg4::CheckSimEnergyDeposit : public art::EDAnalyzer {
+public:
+
+    explicit CheckSimEnergyDeposit(fhicl::ParameterSet const& p);
+    virtual void beginJob();
+    virtual void beginRun(const art::Run& Run);
+    virtual void endJob();
+    virtual void analyze(const art::Event& event);
+
+private:
+
+
+  TH1F* _hnHits;        // number of SimEnergyDepositHits
+  TH1F* _hEdep;        //  average energy deposition in SimEnergyDepositHits
+  TH1F* _hnumPhotons;   //  number of Photons per SimEnergyDepositHits
+  TH1F* _hLandauPhotons;//  Edep/cm  SimEnergyDepositHits
+  TH1F* _hLandauEdep;   //  number of Photons/cm SimEnergyDepositHits
+  TH1F* _hSteplength;   //  Geant 4 step length
+  TNtuple* _ntuple;
+};
+
+#endif	/* CHECKSIMENERGYDEPOSIT_MODULE_HH */
+
