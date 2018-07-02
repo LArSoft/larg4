56e470fca3304abfc1058f53dcdceceeff5192d5
Hans-Joachim Wenzel <wenzel@fnal.gov>
2018-07-02 16:39:29 -0500

    add reading of MCTruth data produced by SingleGen

---
+++ larg4/pluginActions/MCTruthEventAction_service.cc
@@ -0,0 +1,222 @@
+#include "larg4/pluginActions/MCTruthEventAction_service.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "artg4tk/services/ActionHolder_service.hh"
+//#include "artg4tk/DataProducts/EventGenerators/GenParticle.hh"
+//#include "artg4tk/DataProducts/EventGenerators/GenParticleCollection.hh"
+// Geant4  includes
+
+#include "Geant4/G4Event.hh"
+#include "Geant4/G4ParticleTable.hh"
+#include "Geant4/G4IonTable.hh"
+#include "Geant4/G4PrimaryVertex.hh"
+#include "Geant4/G4ParticleDefinition.hh"
+//#include "Geant4/G4HEPEvtInterface.hh"
+#include "nusimdata/SimulationBase/MCTruth.h"
+#include "nusimdata/SimulationBase/MCParticle.h"
+#include "nutools/G4Base/PrimaryParticleInformation.h"
+#include <iostream>
+#include <cmath>
+#include <CLHEP/Vector/LorentzVector.h>
+using std::string;
+
+larg4::MCTruthEventActionService::
+MCTruthEventActionService(fhicl::ParameterSet const & p,
+        art::ActivityRegistry &)
+: PrimaryGeneratorActionBase(p.get<string>("name", "MCTruthEventActionService")),
+// Initialize our message logger
+logInfo_("MCTruthEventActionService") {
+}
+
+// Destructor
+
+larg4::MCTruthEventActionService::~MCTruthEventActionService() {
+
+}
+
+// Create a primary particle for an event!
+// (Standard Art G4 simulation)
+
+void larg4::MCTruthEventActionService::generatePrimaries(G4Event * anEvent) {
+    // For each MCTruth (probably only one, but you never know):
+    // index keeps track of which MCTruth object you are using
+    size_t index = 0;
+    std::map< CLHEP::HepLorentzVector, G4PrimaryVertex* >                  vertexMap;
+    std::map< CLHEP::HepLorentzVector, G4PrimaryVertex* >::const_iterator  vi; 
+    art::ServiceHandle<artg4tk::ActionHolderService> actionHolder;
+    art::Event & evt = actionHolder -> getCurrArtEvent();
+    std::vector< art::Handle< std::vector<simb::MCTruth> > > mclists;
+    evt.getManyByType(mclists);
+    std::cout << "Primary:: MCTRUTH: Size: "<<mclists.size()<<std::endl;
+    for(size_t mcl = 0; mcl < mclists.size(); ++mcl){
+      art::Handle< std::vector<simb::MCTruth> > mclistHandle = mclists[mcl];
+      for(size_t m = 0; m < mclistHandle->size(); ++m){
+        art::Ptr<simb::MCTruth> mct(mclistHandle, m);
+	std::cout << "NParticles: "<< mct->NParticles()<<std::endl;
+	simb::MCParticle particle = mct->GetParticle(0);
+	std::cout << "status code:  " << particle.StatusCode()<<std::endl;
+	if ( particle.StatusCode() != 1 ) continue;
+	// Get the Particle Data Group code for the particle.
+	G4int pdgCode = particle.PdgCode();
+	G4double x = particle.Vx() * CLHEP::cm;
+	G4double y = particle.Vy() * CLHEP::cm;
+	G4double z = particle.Vz() * CLHEP::cm;
+	G4double t = particle.T()  * CLHEP::ns;
+	//std::cout <<"x: "<<x
+	//	  <<"  y: "<<y
+	//	  <<"  z: "<<z
+	//	  <<"  t: "<<t
+	//	  <<std::endl;
+	//std::cout << "Primary:: m  Size: "<<*(mct.get())<<std::endl;
+	simb::MCTruth GHFJ =(simb::MCTruth) *(mct.get());
+	//simb::MCParticle pp = GHFJ.GetParticle(1);
+	//std::cout << "pdgid  "<<pp.PdgCode()<<std::endl;
+
+	//std::cout << "Origin::  " << GHFJ.Origin()<<std::endl;
+	//std::cout << "number::  " << GHFJ.NParticles()<<std::endl;
+	//	std::cout << "Origin::   Size: "<<*(mct.get()).Origin()<<std::endl;
+	// Create a CLHEP four-vector from the particle's vertex.
+	CLHEP::HepLorentzVector fourpos(x,y,z,t);
+      
+	// Is this vertex already in our map?
+	G4PrimaryVertex* vertex = 0;
+	std::map< CLHEP::HepLorentzVector, G4PrimaryVertex* >::const_iterator result = vertexMap.find( fourpos );
+	if ( result == vertexMap.end() ){
+	  // No, it's not, so create a new vertex and add it to the
+	  // map.
+	  vertex = new G4PrimaryVertex(x, y, z, t);
+	  vertexMap[ fourpos ] = vertex;
+
+	  // Add the vertex to the G4Event.
+	  anEvent->AddPrimaryVertex( vertex );
+	}
+	else{
+	  // Yes, it is, so use the existing vertex.
+	  vertex = (*result).second;
+	}
+      
+	// Get additional particle information.
+	TLorentzVector momentum = particle.Momentum(); // (px,py,pz,E)
+	TVector3 polarization = particle.Polarization();
+      
+	// Get the particle table if necessary.  (Note: we're
+	// doing this "late" because I'm not sure at what point
+	// the G4 particle table is initialized in the loading process.
+	if ( fParticleTable == 0 ){
+	  fParticleTable = G4ParticleTable::GetParticleTable();
+	}
+
+	// Get Geant4's definition of the particle.
+	G4ParticleDefinition* particleDefinition;
+      
+	if(pdgCode==0){
+	  particleDefinition = fParticleTable->FindParticle("opticalphoton");
+	}
+	else
+	  particleDefinition = fParticleTable->FindParticle(pdgCode);
+
+	if ( pdgCode > 1000000000) { // If the particle is a nucleus
+	  LOG_DEBUG("ConvertPrimaryToGeant4") << ": %%% Nuclear PDG code = " << pdgCode
+					      << " (x,y,z,t)=(" << x
+					      << "," << y
+					      << "," << z
+					      << "," << t << ")"
+					      << " P=" << momentum.P()
+					      << ", E=" << momentum.E();
+	  // If the particle table doesn't have a definition yet, ask the ion
+	  // table for one. This will create a new ion definition as needed.
+	  if (!particleDefinition) {
+            int Z = (pdgCode % 10000000) / 10000; // atomic number
+            int A = (pdgCode % 10000) / 10; // mass number
+            particleDefinition = fParticleTable->GetIonTable()->GetIon(Z, A, 0.);
+          }
+        }
+
+	// What if the PDG code is unknown?  This has been a known
+	// issue with GENIE.
+	if ( particleDefinition == 0 ){
+	  LOG_DEBUG("ConvertPrimaryToGeant4") << ": %%% Code not found = " << pdgCode;
+	  fUnknownPDG[ pdgCode ] += 1;
+	  continue;
+	}
+      
+	// Create a Geant4 particle to add to the vertex.
+	G4PrimaryParticle* g4particle = new G4PrimaryParticle( particleDefinition,
+							       momentum.Px() * CLHEP::GeV,
+							       momentum.Py() * CLHEP::GeV,
+							       momentum.Pz() * CLHEP::GeV);
+
+	// Add more particle information the Geant4 particle.
+	G4double charge = particleDefinition->GetPDGCharge();
+	g4particle->SetCharge( charge );
+	g4particle->SetPolarization( polarization.x(),
+				     polarization.y(),
+				     polarization.z() );
+      
+	// Add the particle to the vertex.
+	vertex->SetPrimary( g4particle );
+
+	// Create a PrimaryParticleInformation object, and save
+	// the MCTruth pointer in it.  This will allow the
+	// ParticleActionList class to access MCTruth
+	// information during Geant4's tracking.
+	//hjw g4b::PrimaryParticleInformation* primaryParticleInfo = new g4b::PrimaryParticleInformation;
+	//hjw primaryParticleInfo->SetMCTruth( mct, index );
+	  
+	// Save the PrimaryParticleInformation in the
+	// G4PrimaryParticle for access during tracking.
+	//hjwg4particle->SetUserInformation( primaryParticleInfo );
+
+	LOG_DEBUG("ConvertPrimaryToGeant4") << ": %%% primary PDG=" << pdgCode
+					    << ", (x,y,z,t)=(" << x
+					    << "," << y
+					    << "," << z
+					    << "," << t << ")"
+					    << " P=" << momentum.P()
+					    << ", E=" << momentum.E();
+
+      } // for each particle in MCTruth
+      ++index;
+    }
+
+
+}
+
+void larg4::MCTruthEventActionService::addG4Particle(G4Event *event,
+        int pdgId,
+        const G4ThreeVector& pos,
+        double time,
+        double energy,
+        //           double properTime,
+        const G4ThreeVector& mom) {
+    // Create a new vertex
+    //     properTime=0.0;
+    G4PrimaryVertex* vertex = new G4PrimaryVertex(pos, time);
+
+    //    static int const verbosityLevel = 0; // local debugging variable
+    //    if ( verbosityLevel > 0) {
+    //      cout << __func__ << " pdgId   : " <<pdgId << endl;
+    //    }
+
+    //       static bool firstTime = true; // uncomment generate all nuclei ground states
+    //       if (firstTime) {
+    //         G4ParticleTable::GetParticleTable()->GetIonTable()->CreateAllIon();
+    //         firstTime = false;
+    //       }
+
+
+    G4PrimaryParticle* particle =
+            new G4PrimaryParticle(pdgId,
+            mom.x(),
+            mom.y(),
+            mom.z(),
+            energy);
+
+    // Add the particle to the event.
+    vertex->SetPrimary(particle);
+
+    // Add the vertex to the event.
+    event->AddPrimaryVertex(vertex);
+}
+
+using larg4::MCTruthEventActionService;
+DEFINE_ART_SERVICE(MCTruthEventActionService)
---
+++ larg4/pluginActions/MCTruthEventAction_service.h
@@ -0,0 +1,73 @@
+//  MCTruthEventAction is the service that injects particles into
+// the simulation.
+// To use this action, all you need to do is put it in the services section
+// of the configuration file, like this:
+// 
+// services: { 
+//   ...
+//     MCTruthEventAction: {}
+//     ...
+// }
+// Expected parameters:
+// - name (string): A name describing the action service.
+//       Default is 'exampleParticleGun'
+
+
+// Include guard
+#ifndef EXAMPLE_MCTRUTHEVENTACTION_SERVICE_HH
+#define EXAMPLE_MCTRUTHEVENTACTION_SERVICE_HH
+
+// Includes
+#include "fhiclcpp/ParameterSet.h"
+#include "art/Framework/Services/Registry/ActivityRegistry.h"
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
+#include "art/Framework/Services/Registry/ServiceMacros.h"
+#include "art/Framework/Core/EDProducer.h"
+
+#include "Geant4/G4Event.hh"
+#include "Geant4/G4VPrimaryGenerator.hh"
+#include "Geant4/G4VUserPrimaryGeneratorAction.hh"
+#include "Geant4/G4ParticleTable.hh"
+#include "Geant4/globals.hh"
+
+// Get the base class
+#include "artg4tk/actionBase/PrimaryGeneratorActionBase.hh"
+
+namespace larg4 {
+
+  class MCTruthEventActionService 
+    : public artg4tk::PrimaryGeneratorActionBase {
+  public: 
+    MCTruthEventActionService(fhicl::ParameterSet const&, 
+				    art::ActivityRegistry&);
+    virtual ~MCTruthEventActionService();
+    
+    void addG4Particle(G4Event *event,
+                       int pdgId,
+                       const G4ThreeVector& pos,
+                       double time,
+                       double energy,
+                       const G4ThreeVector& mom);
+
+    // To generate primaries, we need to overload the GeneratePrimaries
+    // method. 
+    
+    virtual void generatePrimaries(G4Event * anEvent) override;
+
+    // We don't add anything to the event, so we don't need callArtProduces
+    // or FillEventWithArtStuff.
+
+  private:
+
+    // A message logger for this action object
+    mf::LogInfo logInfo_;
+    static G4ParticleTable*           fParticleTable; ///< Geant4's table of particle definitions.
+    std::map<G4int, G4int>            fUnknownPDG;    ///< map of unknown PDG codes to instances
+  };
+}//namespace larg4
+using larg4::MCTruthEventActionService;
+DECLARE_ART_SERVICE(MCTruthEventActionService,LEGACY)
+
+
+#endif
