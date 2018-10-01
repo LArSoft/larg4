2243575e27a02a70ea55ed25b44c44ec40134d89
Lynn Garren <garren@fnal.gov>
2018-10-01 17:34:35 -0500

    header now in art

--- larg4/pluginActions/ParticleListAction_service.h
+++ larg4/pluginActions/ParticleListAction_service.h
@@ -16,7 +16,7 @@
 #include "messagefacility/MessageLogger/MessageLogger.h"
 #include "art/Framework/Core/ProductRegistryHelper.h"
 #include "art/Framework/Services/Registry/ServiceMacros.h"
-#include "canvas/Persistency/Provenance/ModuleDescription.h"
+#include "art/Persistency/Provenance/ModuleDescription.h"
 
 #include "larsim/LArG4/ParticleFilters.h" // larg4::PositionInVolumeFilter
 #include "nutools/ParticleNavigation/ParticleList.h" // larg4::PositionInVolumeFilter
