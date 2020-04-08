e6e2b36d15ba9559782d80d1cb8b0be287cef68d
Hans <wenzel@fnal.gov>
2020-04-08 14:30:25 -0500

    fix runtime errors,  remove all references to visualization

--- larg4/Core/larg4Main_module.cc
+++ larg4/Core/larg4Main_module.cc
@@ -2,7 +2,7 @@
 
 // larg4Main_module.cc replicates many GEANT programs' @main()@ driver. It
 // creates and initializes the run manager, controls the beginning and end of
-// events, and controls visualization.
+// events.
 
 #include "nusimdata/SimulationBase/MCParticle.h"
 #include "nusimdata/SimulationBase/MCTruth.h"
@@ -40,10 +40,7 @@
 
 #include "lardataalg/MCDumpers/MCDumpers.h"
 
-// G4 includes
-#ifdef G4VIS_USE
-#include "Geant4/G4VisExecutive.hh"
-#endif
+
 
 #include "Geant4/G4UImanager.hh"
 #include "Geant4/G4UIterminal.hh"
@@ -71,20 +68,13 @@ namespace larg4 {
     G4UIsession* session_;
     G4UImanager* UI_;
 
-    // Visualization manager if necessary
-#ifdef G4VIS_USE
-    G4VisManager* visManager_;
-#endif
+
 
 	// Pseudorandom engine seed (originally hardcoded to 12345),
 	// obtained from the configuration file.
 	// Note: the maximum seed value is 9e8, which is potentially larger
 	// than a long can hold.
 	long seed_;
-
-    // Determine whether we should use visualization
-    // False by default, can be set by config file
-    bool enableVisualization_;
 
     // Directory path(s), in colon-delimited list, in which we should look for
     // macros, or the name of an environment variable containing that path.
@@ -96,9 +86,6 @@ namespace larg4 {
     // Initialized based on macroPath_.
     cet::search_path pathFinder_;
 
-    // Name of a macro file for visualization
-    // 'vis.mac' by default, and can be customized by config file.
-    string visMacro_;
 
     // Boolean to determine whether we pause execution after each event
     // If it's true, then we do. Otherwise, we pause only after all events
@@ -106,19 +93,6 @@ namespace larg4 {
     // False by default, can be changed by afterEvent in FHICL
     bool pauseAfterEvent_;
 
-	// Boolean to determine whether we're in "visualize only certain
-	// events" mode. If so, we pause and show the visualization only after
-	// the given events. Turning this on only works if visualization is
-	// also enabled, and it will pause, pass, or bring up a UI at the end
-	// of the given events, as specified by afterEvent.
-	bool visSpecificEvents_;
-
-	// If we're in "visualize only certain events" mode, this vector
-	// contains the events for which the visualization should be displayed.
-	// This is a map because determining whether an event is in there is
-	// O(log(n)), rather than O(n) for a vector, and find(...) is a heck of
-	// a lot more convenient than looping over the vector.
-	std::map<int, bool> eventsToDisplay_;
 
     // Run diagnostic level (verbosity)
     int rmvlevel_;
@@ -145,43 +119,21 @@ namespace larg4 {
 // Constructor - set parameters
 larg4::larg4Main::larg4Main(fhicl::ParameterSet const & p)
   : EDProducer{p},
-    runManager_(),
-    session_(0),
-    UI_(0),
-	seed_(p.get<long>("seed", -1)),
-    enableVisualization_( p.get<bool>("enableVisualization",false)),
-    macroPath_( p.get<std::string>("macroPath",".")),
-    pathFinder_( macroPath_),
-    visMacro_( p.get<std::string>("visMacro", "vis.mac")),
-    pauseAfterEvent_(false),
-	visSpecificEvents_(p.get<bool>("visualizeSpecificEvents",false)),
-	eventsToDisplay_(),
-    rmvlevel_( p.get<int>("rmvlevel",0)),
-    uiAtBeginRun_( p.get<bool>("uiAtBeginRun", false)),
-    uiAtEndEvent_(false),
-    afterEvent_( p.get<std::string>("afterEvent", "pass")),
+  runManager_(),
+  session_(0),
+  UI_(0),
+  seed_(p.get<long>("seed", -1)),
+  macroPath_( p.get<std::string>("macroPath",".")),
+  pathFinder_( macroPath_),
+  pauseAfterEvent_(false),
+  rmvlevel_( p.get<int>("rmvlevel",0)),
+  uiAtBeginRun_( p.get<bool>("uiAtBeginRun", false)),
+  uiAtEndEvent_(false),
+  afterEvent_( p.get<std::string>("afterEvent", "pass")),
   logInfo_("larg4Main")
-//  fSparsifyTrajectories(false),
-//  fparticleListAction(0)
-//  pla_("ParticleListAction")
-
 {
   produces< std::vector<simb::MCParticle> >();
-  //<--produces< art::Assns<simb::MCTruth, simb::MCParticle> >();
   produces< art::Assns<simb::MCTruth, simb::MCParticle, sim::GeneratedParticleInfo> >();
-	// If we're in "visualize specific events" mode (essentially only pause
-	// after given events), then extract the list of events we need to
-	// pause for. They are placed in a map because it is more efficient to
-	// determine whether a given entry is present in the map than a vector.
-	if (visSpecificEvents_) {
-		std::vector<int> eventsToDisplayVec =
-			p.get<vector<int>>("eventsToDisplay");
-		for (size_t i = 0; i < eventsToDisplayVec.size(); i++) {
-			eventsToDisplay_[eventsToDisplayVec[i]] = true;
-		}
-		// Would be nice to have error checking here, but for now, if you
-		// do something silly, it'll probably just crash.
-	}
 
   // We need all of the services to run @produces@ on the data they will store. We do this
   // by retrieving the holder services.
@@ -194,10 +146,6 @@ larg4::larg4Main::larg4Main(fhicl::ParameterSet const & p)
     // And running @callArtProduces@ on each
     actionHolder -> callArtProduces(producesCollector());
     detectorHolder -> callArtProduces(producesCollector());
-
-    //    ((artg4tk::SteppingActionBase*)&*pla)-> callArtProduces(this);
-    // ((artg4tk::EventActionBase*)&*pla) -> callArtProduces(this);
-    // ((artg4tk::TrackingActionBase*)&*pla) -> callArtProduces(this);
   // Set up the random number engine.
   // See the documentation in RandomNumberHeader.h for
   // how this works. Note that @createEngine@ is a member function
@@ -271,46 +219,6 @@ void larg4::larg4Main::beginRun(art::Run & r)
   //get the pointer to the User Interface manager
   UI_ = G4UImanager::GetUIpointer();
 
-  // Set up visualization if it's allowed by current values of env. variables
-#ifdef G4VIS_USE
-
-  // Turn on visualization if necessary
-  if (enableVisualization_) {
-    logInfo_ << "Initializing visualization\n" << endl;
-
-    // Create and initialize the visualization manager
-    visManager_ = new G4VisExecutive;
-    visManager_->Initialize();
-
-    // Find the macro (or try to) along the directory path.
-    string macroLocation = "";
-    bool macroWasFound = pathFinder_.find_file(visMacro_, macroLocation);
-    logInfo_ << "Finding path for " << visMacro_ << "...\nSearch "
-	     << (macroWasFound ? "successful " : "unsuccessful ")
-	     << "and path is: \n" << macroLocation << "\n" << endl;
-
-    // Execute the macro if we were able to find it
-    if (macroWasFound) {
-      // Create the string containing the execution command
-      logInfo_ << "Executing macro: " << visMacro_ << "\n" << endl;
-      string commandToExecute = "/control/execute ";
-      commandToExecute.append(macroLocation);
-      UI_->ApplyCommand(commandToExecute);
-
-    } else {
-      // If it wasn't found...
-      // Leave a message for the user ...
-      logInfo_ << "Unable to find " << visMacro_ << " in the path(s) "
-	       << macroPath_ << endl;
-      // ... and disable visualization for the future
-      enableVisualization_ = false;
-      delete visManager_;
-
-    } // if the macro was found
-
-  } // if visualization was enabled
-
-#endif // G4VIS_USE
 
   // Open a UI if asked
   if ( uiAtBeginRun_ ) {
@@ -347,85 +255,6 @@ void larg4::larg4Main::produce(art::Event & e)
   auto &tpassn = pla->GetAssnsMCTruthToMCParticle();
   e.put(std::move(partCol));
   e.put(std::move(tpassn));
-
-/*
-  unsigned int nGeneratedParticles = 0;
-  art::ServiceHandle<larg4::ParticleListActionService const> h;
-  sim::ParticleList particleList =h->YieldList();
-
-  std::unique_ptr< std::vector<simb::MCParticle> >               partCol                    (new std::vector<simb::MCParticle  >);
-
-  auto tpassn = std::make_unique<art::Assns<simb::MCTruth, simb::MCParticle, sim::GeneratedParticleInfo>>();
-        for(auto const& partPair: particleList) {
-          simb::MCParticle& p = *(partPair.second);
-          ++nGeneratedParticles;
-
-          // if the particle has been marked as dropped, we don't save it
-          // (as of LArSoft ~v5.6 this does not ever happen because
-          // ParticleListAction has already taken care of deleting them)
-          //if (ParticleListAction::isDropped(&p)) continue;
-
-          sim::GeneratedParticleInfo const truthInfo{
-            fparticleListAction->GetPrimaryTruthIndex(p.TrackId())
-            };
-          if (!truthInfo.hasGeneratedParticleIndex() && (p.Mother() == 0)) {
-            // this means it's primary but with no information; logic error!!
-            art::Exception error(art::errors::LogicError);
-            error << "Failed to match primary particle:\n";
-            sim::dump::DumpMCParticle(error, p, "  ");
-	    //            error << "\nwith particles from the truth record '"
-            //  << mclistHandle.provenance()->inputTag() << "':\n";
-	    //            sim::dump::DumpMCTruth(error, *mct, 2U, "  "); // 2 points per line
-            //error << "\n";
-            throw error;
-          }
-
-	  if(fSparsifyTrajectories) p.SparsifyTrajectory();
-
-          partCol->push_back(std::move(p));
-
-          tpassn->addSingle(mct, makeMCPartPtr(partCol->size() - 1), truthInfo);
-
-        } // for(particleList)
-
-  */
-
-
-#ifdef G4VIS_USE
-  // If visualization is enabled, and we want to pause after each event, do
-  // the pausing.
-  if (enableVisualization_) {
-
-    // Flush the visualization
-    //UI_->ApplyCommand("/tracking/storeTrajectory 1");
-    UI_->ApplyCommand("/vis/viewer/flush");
-
-    // Only pause or bring up a UI if
-	//  a) we're doing so for all events (!visSpecificEvents_)
-	//  b) the current event was specified as one to pause for
-	//     (eventsToDisplay_.count(e.id().event()) > 0)
-	if ( !visSpecificEvents_ || eventsToDisplay_.count(e.id().event()) > 0 ) {
-		if ( uiAtEndEvent_ ) {
-			session_ = new G4UIterminal;
-			session_->SessionStart();
-			delete session_;
-		}
-
-		if ( pauseAfterEvent_) {
-			// Use cout so that it is printed to console immediately.
-			// logInfo_ prints everything at once, so if we used that, we
-			// would find out that we should press ENTER to continue only
-			// *after* we'd actually done so!
-			cout << "Event: " << e.id().event()
-				<< ", pausing so you can appreciate visualization. "
-				<< "Hit ENTER to continue." << std::endl;
-			std::cin.ignore();
-		}
-	}
-
-  }
-#endif
-
 }
 
 // At end run
@@ -433,16 +262,7 @@ void larg4::larg4Main::endRun(art::Run & r)
 {
   art::ServiceHandle<ActionHolderService> actionHolder;
   actionHolder->setCurrArtRun(r);
-
   runManager_ -> BeamOnEndRun();
-
-  //  visualization stuff
-#ifdef G4VIS_USE
-  if ( enableVisualization_ ) {
-    // Delete ui
-    delete visManager_;
-  }
-#endif
 }
 
 using larg4::larg4Main;
