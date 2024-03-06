cf083ae45a49bf25a8963f0f3978a5f6bfdf2efa
Lynn Garren <garren@fnal.gov>
2024-03-06 16:53:32 -0600

    using updated clang format

--- larg4/Services/SimEnergyDepositSD.cc
+++ larg4/Services/SimEnergyDepositSD.cc
@@ -40,7 +40,10 @@ namespace larg4 {
 
   //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-  void SimEnergyDepositSD::Initialize(G4HCofThisEvent* HCE) { hitCollection.clear(); }
+  void SimEnergyDepositSD::Initialize(G4HCofThisEvent* HCE)
+  {
+    hitCollection.clear();
+  }
   //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
   G4bool SimEnergyDepositSD::ProcessHits(G4Step* aStep, G4TouchableHistory*)
